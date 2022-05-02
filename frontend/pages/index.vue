549f37569f39df107eab3a178f7d84a6fe4ad16c
Valentina Cares <valentina.cares@usach.cl>
2022-05-02 19:58:56 -0400

    Valentina Cares - Modificación en index.vue y creación de tabla.vue

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,4 +1,19 @@
 <template>
-  <h1>Visualizador de tareas</h1>
-  
+  <h1 class="title">
+    Listado de tareas
+  </h1>
+
 </template>
+
+<style>
+body {
+  background-color:#4FBDBA;
+}
+
+.title {
+  font: bold 200% monospace;
+  text-align: center;
+  color: #072227
+}
+
+</style>
