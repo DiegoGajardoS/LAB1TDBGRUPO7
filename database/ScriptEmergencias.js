3075cd85893c85ada666e831986f3e6ba4495d5c
carolina <carito.antillanca@gmail.com>
2022-07-11 20:10:44 -0400

    [Carolina Antillanca] - Scripts emergencias y tareas

---
+++ database/ScriptEmergencias.js
@@ -0,0 +1,45 @@
+
+
+[{
+    "_id": 0,
+    "titulo": "Tsumani",
+    "direccion": "Valdivia",
+    "descripcion": "Embarcaciones en problemas",
+    "id_institucion": 1,
+    "activo": true,
+    "latitud": -73.255457,
+    "longitud": -39.835746,
+    "deleted": false
+},{
+    "_id": 1,
+    "titulo": "Incendio",
+    "direccion": "Santiago",
+    "descripcion": "Cientos de casas destruidas",
+    "id_institucion": 2,
+    "activo": true,
+    "latitud": -70.665808,
+    "longitud": -33.418902,
+    "deleted": false
+},{
+    "_id": 2,
+    "titulo": "Inundacion",
+    "direccion": "Coquimbo",
+    "descripcion": "Urgente",
+    "id_institucion": 1,
+    "activo": true,
+    "latitud": -71.358944,
+    "longitud": -29.987806,
+    "deleted": false
+},{
+    "_id": 3,
+    "titulo": "Erupcion volcanica",
+    "direccion": "Villarica",
+    "descripcion": "destruccion total",
+    "id_institucion": 3,
+    "activo": true,
+    "latitud": -71.942540,
+    "longitud": -39.418783,
+    "deleted": false
+}]
+
+
---
+++ database/ScriptTareas.js
@@ -0,0 +1,42 @@
+
+[{
+    "_id": 0,
+    "titulo": "Limpiar",
+    "descripcion": "Sacar Escombros",
+    "id_estado": 1,
+    "id_emergencia": 0,
+    "cant_voluntarios": 20,
+    "latitud": -73.255457,
+    "longitud": -39.835746,
+    "deleted": false
+},{
+    "_id": 1,
+    "titulo": "Ordenar suministros",
+    "descripcion": "Repartir las donaciones para que sean entregadas a los damnificados",
+    "id_estado": 1,
+    "id_emergencia": 1,
+    "cant_voluntarios": 10,
+    "latitud": -70.665808,
+    "longitud": -33.418902,
+    "deleted": false
+},{
+    "_id": 2,
+    "titulo": "Guiar refugiados",
+    "descripcion": "Asignar lugares de refugio a los afectados por la emergencia",
+    "id_estado": 1,
+    "id_emergencia": 2,
+    "cant_voluntarios": 15,
+    "latitud": -71.358944,
+    "longitud": -29.987806,
+    "deleted": false
+},{
+    "_id": 3,
+    "titulo": "Cuidar y alimentar mascotas",
+    "descripcion": "Cuidar a las mascotas que esten afectadas por la emergencia",
+    "id_estado": 1,
+    "id_emergencia": 3,
+    "cant_voluntarios": 5,
+    "latitud": -71.942540,
+    "longitud": -39.418783,
+    "deleted": false
+}]
