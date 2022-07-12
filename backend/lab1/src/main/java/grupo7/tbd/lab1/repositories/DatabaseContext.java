3d30e4b6b978cbb4be8f9bcab6555b014fbfca0f
Valentina Cares <valentina.cares@usach.cl>
2022-07-11 20:42:02 -0400

    [Valentina Cares] - Archivo DatabaseContext

--- backend/lab1/src/main/java/grupo7/tbd/lab1/repositories/DatabaseContext.java
+++ backend/lab1/src/main/java/grupo7/tbd/lab1/repositories/DatabaseContext.java
@@ -6,12 +6,32 @@ package grupo7.tbd.lab1.repositories;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.sql2o.Sql2o;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.bson.codecs.configuration.CodecRegistries;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class DatabaseContext {
+
     @Bean
     public Sql2o sql2o(){
         return new Sql2o("jdbc:postgresql://127.0.0.1:5432/lab1", "postgres", "TBD2022");
+    }
+    @Bean
+    MongoDatabase database() {
+        CodecRegistry defaultCodecRegistry = MongoClientSettings.getDefaultCodecRegistry();
+        CodecRegistry fromProvider = CodecRegistries.fromProviders(PojoCodecProvider.builder().automatic(true).build());
+        CodecRegistry pojoCodecRegistry = CodecRegistries.fromRegistries(defaultCodecRegistry, fromProvider);
+
+        // Cliente de base de datos
+        MongoClient mongoClient = MongoClients.create();
+        MongoDatabase database = mongoClient.getDatabase("TBD-LAB3").withCodecRegistry(pojoCodecRegistry);
+        return database;
     }
 
 }
